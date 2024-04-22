801fa138a01fbb83d39b0346bb13f056151976c2
Simon Piihl Rasmussen <61151@edu.campusvejle.dk>
2024-04-22 11:42:31 +0200

    Bunch of test stuff

--- Oop.Encapsulation/Program.cs
+++ Oop.Encapsulation/Program.cs
@@ -1,2 +1,35 @@
 ﻿using System;
 
+namespace Oop.Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            subject subject = new subject();
+
+            try
+            {
+                subject.Name = "Math";
+                subject.Code = "CJG-678";
+                subject.Teacher = "John Cena";
+                subject.EctsPoints = 5;
+                subject.StartDate = 2021;
+                subject.EndDate = new DateTime(2021, 12, 31);
+                subject.ExamDate = new DateTime(2021, 12, 31);
+
+                Console.WriteLine($"Subject: {subject.Name}");
+                Console.WriteLine($"Code: {subject.Code}");
+                Console.WriteLine($"Teacher: {subject.Teacher}");
+                Console.WriteLine($"ECTS Points: {subject.EctsPoints}");
+                Console.WriteLine($"Start Date: {subject.StartDate}");
+                Console.WriteLine($"End Date: {subject.EndDate}");
+                Console.WriteLine($"Exam Date: {subject.ExamDate}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Oop.Encapsulation/subject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Oop.Encapsulation
+{
+    internal class subject
+    {
+        private string name;
+        public string Name
+        {
+            get { 
+                return name;
+            }
+            set {
+                if (string.IsNullOrEmpty(value) || Regex.IsMatch(value, @"\d") || value.Length < 4)
+                {
+                    throw new ArgumentException("Name must be atleast 4 characters long. Name cannot be a number, null or empty");
+                }
+                name = value;
+            }
+        }
+
+        private string code;
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        private string teacher;
+        public string Teacher
+        {
+            get { return teacher; }
+            set { teacher = value; }
+        }
+
+        private int ectsPoints;
+        public int EctsPoints
+        {
+        get { return ectsPoints; }
+        set { ectsPoints = value; }
+        }
+
+        private int startDate;
+        public int StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        private DateTime endDate;
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        private DateTime examDate;
+        public DateTime ExamDate
+        {
+            get { return examDate; }
+            set { examDate = value; }
+        }
+    }
+}
